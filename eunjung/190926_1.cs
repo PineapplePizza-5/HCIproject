2381bc04d2c69eef3dbf507bad00822bb2becb5b
최은정 <morned270@daum.net>
2019-09-26 14:27:59 +0900

    Add 190926_1, 2

---
+++ eunjung/190926_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public void swap(ref int x, ref int y)
+        {
+            int temp;
+
+            temp = x;
+            x = y;
+            y = temp;
+        
+        }
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Program app = new Program();
+            Console.WriteLine("X = {0}, Y = {1}", x, y);
+            app.swap(ref x, ref y);
+            Console.WriteLine("X = {0}, Y = {1}", x, y);
+        }
+    }
+}
---
+++ eunjung/190926_2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Student
+    {
+        protected int korean;
+        protected int english;
+        protected int math;
+        protected double average;
+
+        //set만들기
+        public void SetScore()
+        {
+            Console.Write("국어 성적 : ");
+            this.korean = int.Parse(Console.ReadLine());
+            Console.Write("수학 성적 : ");
+            this.math = int.Parse(Console.ReadLine());
+            Console.Write("영어 성적 : ");
+            this.english = int.Parse(Console.ReadLine());
+
+            Console.Write("\n");
+
+        }
+
+        //get만들기
+        public double GetAverage()
+        {
+            average = (double)((this.korean + this.math + this.english) / 3);
+            return this.average;
+        }
+
+
+
+    }
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Student[] sList;
+            sList = new Student[3];         //sList를 가리킬 3개의 변수 생성
+
+            for (int i = 0; i < 3; i++) 
+            {
+                sList[i] = new Student();
+                Console.WriteLine("Student? {0}", i);
+                sList[i].SetScore();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Average = {0:f2}", sList[i].GetAverage());
+
+            }
+
+        }
+    }
+}
