9f41c3be94fcb93551f4a107952ae6e8be513822
EunjiShin <38103085+EunjiShin@users.noreply.github.com>
2019-09-26 13:38:19 +0900

    20190926_swap

--- eunji/Program.cs
+++ eunji/Program.cs
@@ -4,82 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace grade_3student
+namespace swap_program
 {
-
-    class Student
+    class Program
     {
-        public int kor;
-        public int math;
-        public int eng;
-        public int ID;
+        public void swap(ref int x, ref int y) {
+            int temp = x;
+            x = y;
+            y = temp;
 
-
-        public Student(int student_ID) {
-            ID = student_ID;
-            Console.WriteLine(ID + "번 학생의 점수 입니다.");
         }
 
-        public void setScore()
+        static void Main(string[] args)
         {
-            Console.Write("국어성적 : ");
-            kor = int.Parse(Console.ReadLine());
-            Console.Write("수학성적 : ");
-            math = int.Parse(Console.ReadLine());
-            Console.Write("영어성적 : ");
-            eng = int.Parse(Console.ReadLine());
+            int x = 10;
+            int y = 20;
+
+            Program app = new Program();
+            app.swap(ref x, ref y);
+            Console.WriteLine("x = {0}, y = {1}", x, y);
+            Console.Read();
         }
-        }
-
-    class Average
-    {
-        public int kor_sum = 0;
-        public int math_sum = 0;
-        public int eng_sum = 0;
-
-        public int kor_average;
-        public int math_average;
-        public int eng_average;
-
-
-        public void getAverage(Student[] student)
-        {
-
-            for (int i = 0; i < 3; i++)
-            {
-                kor_sum += (student[i].kor);
-                math_sum += (student[i].math);
-                eng_sum += (student[i].eng);
-            }
-
-            kor_average = kor_sum / 3;
-            math_average = math_sum / 3;
-            eng_average = eng_sum / 3;
-
-            Console.Write("국어 과목의 평균은 : = {0} ", kor_average);
-            Console.Write("수학 과목의 평균은 : = {0} ", math_average);
-            Console.Write("영어 과목의 평균은 : = {0} ", eng_average);
-
-            Console.ReadLine();
-        }
-
     }
-
-        class Program
-        {
-            static void Main(string[] args)
-            {
-            Student[] student;
-            Average score = new Average();
-            student = new Student[3];
-            for (int i = 0; i < 3; i++) {
-                student[i] = new Student(i+1);
-                student[i].setScore();
-            }
-            score.getAverage(student);
-
-            }
-        }
-
-
-    }
+}
